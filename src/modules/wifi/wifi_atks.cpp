96f78430403bae231dd3bedac550d05ed0cd8e78
duykhangks6-art <duykhangks6@gmail.com>
2026-06-06 14:41:33 +0000

    xoá

--- src/modules/wifi/wifi_atks.cpp
+++ src/modules/wifi/wifi_atks.cpp
@@ -717,14 +717,8 @@ AGAIN:
     options = {
         {"Information",         [=]() { wifi_atk_info(tssid, mac, channel); }      },
         {"Deauth",              [=]() { target_atk(tssid, mac, channel); }         },
-#ifndef LITE_VERSION
         {"Capture Handshake",   [=]() { capture_handshake(tssid, mac, channel); }  },
-#endif
-        {"Clone Portal",        [=]() { EvilPortal(tssid, channel, false, false); }},
-        {"Deauth+Clone",        [=]() { EvilPortal(tssid, channel, true, false); } },
-        {"Deauth+Clone+Verify",
-         [=]() // New WiFi Attack
-         { EvilPortal(tssid, channel, true, true); }                               },
+
     };
     addOptionToMainMenu();
 
